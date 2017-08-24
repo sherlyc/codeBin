046f866614dc2c4f816c63c2de1bc9fe02edb5cf
Sherly Chan <sherlyc@gmail.com>
2017-08-24 20:10:22 +1200

    Create string-repeatify.js

---
+++ string-repeatify.js
@@ -0,0 +1,9 @@
+//Write down a function which after triggering like this:
+//console.log('hello'.repeatify(3))
+//Should print hellohellohello.
+
+String.prototype.repeatify = function (count) {
+  return this.repeat(count)
+}
+console.log('hello'.repeatify(3)) //hellohellohello
+
