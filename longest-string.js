4e27a355df0cb388074b1b348831abae172bd408
Sherly Chan <sherlyc@gmail.com>
2017-08-24 19:52:45 +1200

    Create longest-string.js

---
+++ longest-string.js
@@ -0,0 +1,15 @@
+const longestString = (arr) => {
+  let longestLength = 0;
+  let returnString = "";
+
+  arr.forEach((item) => {
+    if ((typeof item === 'string') && (item.length > longestLength)) {
+      longestLength = item.length;
+      returnString = item;
+    }
+  })
+
+  return returnString;
+}
+let arr = ['hello', 'children', 'how can I tell you', 'wakapupu wakamai']
+console.log(longestString(arr))
