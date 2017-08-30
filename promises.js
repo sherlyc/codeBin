19bbb21ba232e52733c3f0af0a3b329322c602d9
Sherly Chan <sherlyc@gmail.com>
2017-08-30 22:12:54 +1200

    Create promises.js

---
+++ promises.js
@@ -0,0 +1,10 @@
+var promise = new Promise(function(resolve, reject) {
+  // do a thing, possibly async, then…
+
+  if (/* everything turned out fine */) {
+    resolve("Stuff worked!");
+  }
+  else {
+    reject(Error("It broke"));
+  }
+});
